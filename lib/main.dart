28649350288bcbbc2ba01a232fd5bc63f8ae74ef
Anish <sunaranish17@gmail.com>
2021-08-13 11:27:31 +0545

    Feat: Scaffold widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main() {}
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("First Flutter App"),
+        ),
+        body: Text("This is my first flutter app"),
+      ),
+    );
   }
 }
