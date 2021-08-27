8bb02e54c0baf4638fcbe6916daa9326b530a6b8
Anish <sunaranish17@gmail.com>
2021-08-27 09:55:58 +0545

    Feat: Create post container completed

--- lib/widgets/create_post_container.dart
+++ lib/widgets/create_post_container.dart
@@ -14,43 +14,92 @@ class CreatePostContainer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.fromLTRB(12.0, 8.0, 12.0, 0.0),
+      color: Colors.white,
       child: Column(
         children: [
-          Row(
-            children: [
-              CircleAvatar(
-                radius: 20.0,
-                backgroundColor: Colors.grey[200],
-                backgroundImage:
-                    CachedNetworkImageProvider(currentUser.imageUrl),
-              ),
-              const SizedBox(
-                width: 8.0,
-              ),
-              Expanded(
-                child: TextField(
-                  decoration: InputDecoration.collapsed(
-                      hintText: "What\'s on your mind, ${currentUser.name}?"),
+          Container(
+            height: 40.0,
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 20.0,
+                  backgroundColor: Colors.grey[200],
+                  backgroundImage:
+                      CachedNetworkImageProvider(currentUser.imageUrl),
                 ),
-              )
-            ],
+                const SizedBox(
+                  width: 8.0,
+                ),
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration.collapsed(
+                        hintText: "What\'s on your mind, ${currentUser.name}?"),
+                  ),
+                )
+              ],
+            ),
           ),
           const Divider(
             height: 10.0,
             thickness: 0.5,
           ),
           Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Column(
-                children: [
-                  IconButton(
-                    onPressed: () => print("Live"),
-                    icon: Icon(Icons.video_camera_front),
-                    color: Colors.green,
+              ElevatedButton.icon(
+                onPressed: () => print("Live"),
+                icon: Icon(
+                  Icons.videocam,
+                  color: Colors.red,
+                ),
+                label: Text(
+                  "Live",
+                  style: TextStyle(
+                    color: Colors.black,
                   ),
-                  Text("Live"),
-                ],
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.white,
+                ),
               ),
+              const VerticalDivider(
+                width: 8.0,
+              ),
+              ElevatedButton.icon(
+                onPressed: () => print("Photo"),
+                icon: Icon(
+                  Icons.photo_library,
+                  color: Colors.green,
+                ),
+                label: Text(
+                  "Photo",
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.white,
+                ),
+              ),
+              const VerticalDivider(
+                width: 8.0,
+              ),
+              ElevatedButton.icon(
+                onPressed: () => print("Room"),
+                icon: Icon(
+                  Icons.video_call,
+                  color: Colors.purpleAccent,
+                ),
+                label: Text(
+                  "Room",
+                  style: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.white,
+                ),
+              )
             ],
           )
         ],
