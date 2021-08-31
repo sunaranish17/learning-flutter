b251d49e8e915b17bd93a2578d3c48da32b36a06
Anish <sunaranish17@gmail.com>
2021-08-31 14:35:53 +0545

    Feat: Post like comment and share button

--- lib/widgets/post_container.dart
+++ lib/widgets/post_container.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:learning_flutter/config/palette.dart';
 import 'package:learning_flutter/models/models.dart';
 import 'package:learning_flutter/widgets/widgets.dart';
 
@@ -47,11 +48,12 @@ class PostContainer extends StatelessWidget {
                 )
               : const SizedBox.shrink(),
           Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: 12.0,
-            ),
-            // child:
-          )
+              padding: EdgeInsets.symmetric(
+                horizontal: 12.0,
+              ),
+              child: _PostStats(
+                post: post,
+              ))
         ],
       ),
     );
@@ -107,6 +109,127 @@ class _PostHeader extends StatelessWidget {
           ),
         ),
       ],
+    );
+  }
+}
+
+class _PostStats extends StatelessWidget {
+  final Post post;
+
+  const _PostStats({Key? key, required this.post}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(4.0),
+              decoration: BoxDecoration(
+                color: Palette.facebookBlue,
+                shape: BoxShape.circle,
+              ),
+              child: const Icon(
+                Icons.thumb_up,
+                size: 10.0,
+              ),
+            ),
+            const SizedBox(
+              width: 4.0,
+            ),
+            Expanded(
+              child: Text(
+                '${post.likes} likes',
+                style: TextStyle(color: Colors.grey[600]),
+              ),
+            ),
+            Text(
+              '${post.comments} comments',
+              style: TextStyle(color: Colors.grey[600]),
+            ),
+            const SizedBox(
+              width: 8.0,
+            ),
+            Text(
+              '${post.shares} shares',
+              style: TextStyle(color: Colors.grey[600]),
+            )
+          ],
+        ),
+        const Divider(),
+        Row(
+          children: [
+            _PostButton(
+              icon: Icon(
+                Icons.thumb_up_outlined,
+                color: Colors.grey[600],
+                size: 20.0,
+              ),
+              label: "Like",
+              onTap: () => print("Like"),
+            ),
+            _PostButton(
+              icon: Icon(
+                Icons.comment,
+                color: Colors.grey[600],
+                size: 20.0,
+              ),
+              label: "Comment",
+              onTap: () => print("Comment"),
+            ),
+            _PostButton(
+              icon: Icon(
+                Icons.share,
+                color: Colors.grey[600],
+                size: 20.0,
+              ),
+              label: "Share",
+              onTap: () => print("Share"),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class _PostButton extends StatelessWidget {
+  final Icon icon;
+  final String label;
+  final VoidCallback onTap;
+
+  const _PostButton({
+    Key? key,
+    required this.icon,
+    required this.label,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Material(
+        child: InkWell(
+          onTap: onTap,
+          child: Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: 12.0,
+            ),
+            height: 25.0,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                icon,
+                const SizedBox(
+                  width: 4.0,
+                ),
+                Text(label),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
