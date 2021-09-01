debfde24aeeb882c02b4b455fb6702d8f63dc935
Anish <sunaranish17@gmail.com>
2021-09-01 15:34:16 +0545

    Feat: Updates on home screen

--- lib/dio_implementation/home_page.dart
+++ lib/dio_implementation/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:learning_flutter/dio_implementation/models/data.dart';
 import 'package:learning_flutter/dio_implementation/models/user.dart';
 import 'package:learning_flutter/dio_implementation/utils/dio_client.dart';
+import 'package:learning_flutter/dio_implementation/widgets/widgets.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -16,36 +17,91 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text("User Info (Dio Implementation)"),
       ),
-      body: Center(
-          child: FutureBuilder<User?>(
-        future: _client.getUser(id: '7'),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            User? userInfo = snapshot.data;
-            if (userInfo != null) {
-              Data userData = userInfo.data;
-              return Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Image.network(userData.avatar),
-                  SizedBox(
-                    height: 8.0,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'POST',
+                style: TextStyle(
+                  fontSize: 22.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              CreateUser(),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0, bottom: 8.0),
+                child: Text(
+                  'GET & DELETE',
+                  style: TextStyle(
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.bold,
                   ),
-                  Text(
-                    '${userInfo.data.firstName} ${userInfo.data.lastName}',
-                    style: TextStyle(fontSize: 16.0),
+                ),
+              ),
+              FetchUser(),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: Text(
+                  'UPDATE',
+                  style: TextStyle(
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.bold,
                   ),
-                  Text(
-                    userData.email,
-                    style: TextStyle(fontSize: 16.0),
+                ),
+              ),
+              UpdateUser(),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0, bottom: 8.0),
+                child: Text(
+                  'GET (auto fetch)',
+                  style: TextStyle(
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.bold,
                   ),
-                ],
-              );
-            }
-          }
-          return CircularProgressIndicator();
-        },
-      )),
+                ),
+              ),
+              Center(
+                child: FutureBuilder<User?>(
+                  future: _client.getUser(id: '1'),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      User? userInfo = snapshot.data;
+
+                      print("this is userInfo $userInfo");
+
+                      if (userInfo != null) {
+                        Data userData = userInfo.data;
+
+                        return Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Image.network(userData.avatar),
+                            SizedBox(height: 8.0),
+                            Text(
+                              '${userInfo.data.firstName} ${userInfo.data.lastName}',
+                              style: TextStyle(fontSize: 16.0),
+                            ),
+                            Text(
+                              userData.email,
+                              style: TextStyle(fontSize: 16.0),
+                            ),
+                          ],
+                        );
+                      }
+                    } else {
+                      return Text("No Data for the ID, Please use another ID");
+                    }
+                    return CircularProgressIndicator();
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
